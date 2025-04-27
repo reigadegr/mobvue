f580123fea28b247674b3afc169efe10fb40a181
reigadegr <2722688642@qq.com>
2025-04-28 02:09:41 +0800

    fix: 错误

--- src/pages/login/apis/index.ts
+++ src/pages/login/apis/index.ts
@@ -1,5 +1,5 @@
 import type * as Auth from "./type"
-import { request, request2 } from "@/http/axios"
+import { request2 } from "@/http/axios"
 /** 登录并返回 Token */
 export function loginApi(data: Auth.LoginRequestData) {
   return request2<Auth.LoginResponseData>({

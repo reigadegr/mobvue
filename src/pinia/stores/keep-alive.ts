3d81e817b3aaf86b0ff91db7a98d3aead5202f00
reigadegr <2722688642@qq.com>
2025-04-28 02:15:19 +0800

    chore: 改环境变量 && 移除不用函数

--- src/pinia/stores/keep-alive.ts
+++ src/pinia/stores/keep-alive.ts
@@ -19,11 +19,3 @@ export const useKeepAliveStore = defineStore("keep-alive", () => {
 
   return { cachedRoutes, addCachedRoute, delAllCachedRoutes }
 })
-
-/**
- * @description 在 SPA 应用中可用于在 pinia 实例被激活前使用 store
- * @description 在 SSR 应用中可用于在 setup 外使用 store
- */
-export function useKeepAliveStoreOutside() {
-  return useKeepAliveStore(pinia)
-}
